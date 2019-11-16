aae0bc51353a8d831ced7fdf00175c3f6b712758
a.sorokin59@yandex.ru <Mesuti123>
2019-11-16 23:10:01 +0500

    add babel

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,3 @@
-$(function(){
-
-
-
-  
-});
+[1,2,3].map(n => n**2);
+let a = 2;
+console.log('Привет ${a}')
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 let gulp = require('gulp'),
     sass = require('gulp-sass'),
     browserSync = require('browser-sync'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
     rename = require('gulp-rename'),
     del = require('del'),
-    autoprefixer = require('gulp-autoprefixer');
-
+    autoprefixer = require('gulp-autoprefixer'),
+    babel = require('gulp-babel');
 
 gulp.task('clean', async function(){
   del.sync('dist')
@@ -16,20 +14,11 @@ gulp.task('scss', function(){
   return gulp.src('app/scss/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(autoprefixer({
-      browsers: ['last 8 versions']
+        overrideBrowserslist:  ['last 2 versions'],
+        cascade: false
     }))
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({stream: true}))
-});
-
-gulp.task('css', function(){
-  return gulp.src([
-    'node_modules/normalize.css/normalize.css',
-    'node_modules/slick-carousel/slick/slick.css',
-  ])
-    .pipe(concat('_libs.scss'))
-    .pipe(gulp.dest('app/scss'))
     .pipe(browserSync.reload({stream: true}))
 });
 
@@ -41,16 +30,6 @@ gulp.task('html', function(){
 gulp.task('script', function(){
   return gulp.src('app/js/*.js')
   .pipe(browserSync.reload({stream: true}))
-});
-
-gulp.task('js', function(){
-  return gulp.src([
-    'node_modules/slick-carousel/slick/slick.js'
-  ])
-    .pipe(concat('libs.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('app/js'))
-    .pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('browser-sync', function() {
@@ -69,13 +48,11 @@ gulp.task('export', function(){
     .pipe(gulp.dest('dist/css'));
 
   let BuildJs = gulp.src('app/js/**/*.js')
+      .pipe(babel({
+          presets: ['@babel/env']
+      }))
     .pipe(gulp.dest('dist/js'));
-    
-  let BuildFonts = gulp.src('app/fonts/**/*.*')
-    .pipe(gulp.dest('dist/fonts'));
 
-  let BuildImg = gulp.src('app/img/**/*.*')
-    .pipe(gulp.dest('dist/img'));   
 });
 
 gulp.task('watch', function(){
@@ -85,5 +62,4 @@ gulp.task('watch', function(){
 });
 
 gulp.task('build', gulp.series('clean', 'export'))
-
-gulp.task('default', gulp.parallel('css' ,'scss', 'js', 'browser-sync', 'watch'));
+gulp.task('default', gulp.parallel('scss', 'browser-sync', 'watch'));
