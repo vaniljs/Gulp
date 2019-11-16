e030836fdba8b9903533a1b325c852791d25ee5d
a.sorokin59@yandex.ru <Mesuti123>
2019-11-17 01:42:18 +0500

    add cssnano, because sass compresed is bad minify. css export directly

---
+++ dist/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+[1, 2, 3].map(function (n) {
+  return Math.pow(n, 2);
+});
+var a = 2;
+console.log('Привет ${a}');
--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,66 @@
 let gulp = require('gulp'),
     sass = require('gulp-sass'),
+    cssnano = require('gulp-cssnano'),
     browserSync = require('browser-sync'),
     rename = require('gulp-rename'),
     del = require('del'),
     autoprefixer = require('gulp-autoprefixer'),
     babel = require('gulp-babel');
 
-gulp.task('clean', async function(){
-  del.sync('dist')
-})
+sass.compiler = require('node-sass');
 
-gulp.task('scss', function(){
-  return gulp.src('app/scss/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(autoprefixer({
-        overrideBrowserslist:  ['last 2 versions'],
-        cascade: false
-    }))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({stream: true}))
+gulp.task('clean', async function () {
+    del.sync('dist')
 });
 
-gulp.task('html', function(){
-  return gulp.src('app/*.html')
-  .pipe(browserSync.reload({stream: true}))
+gulp.task('sass', function () {
+    //return gulp.src('src/scss/**/*.scss')
+    return gulp.src('src/scss/**/*.sass')
+        .pipe(sass())
+        .pipe(cssnano())
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('script', function(){
-  return gulp.src('app/js/*.js')
-  .pipe(browserSync.reload({stream: true}))
+gulp.task('html', function () {
+    return gulp.src('src/*.html')
+        .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('browser-sync', function() {
-  browserSync.init({
-      server: {
-          baseDir: "app/"
-      }
-  });
+gulp.task('script', function () {
+    return gulp.src('src/js/*.js')
+        .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('export', function(){
-  let buildHtml = gulp.src('app/**/*.html')
-    .pipe(gulp.dest('dist'));
-
-  let BuildCss = gulp.src('app/css/**/*.css')
-    .pipe(gulp.dest('dist/css'));
-
-  let BuildJs = gulp.src('app/js/**/*.js')
-      .pipe(babel({
-          presets: ['@babel/env']
-      }))
-    .pipe(gulp.dest('dist/js'));
-
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: "src/"
+        }
+    });
 });
 
-gulp.task('watch', function(){
-  gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
-  gulp.watch('app/*.html', gulp.parallel('html'))
-  gulp.watch('app/js/*.js', gulp.parallel('script'))
+gulp.task('export', function () {
+    let buildHtml = gulp.src('src/**/*.html')
+        .pipe(gulp.dest('dist'));
+
+    let BuildJs = gulp.src('src/js/**/*.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('build', gulp.series('clean', 'export'))
-gulp.task('default', gulp.parallel('scss', 'browser-sync', 'watch'));
+gulp.task('watch', function () {
+    gulp.watch('src/scss/**/*.sass', gulp.parallel('sass'));
+    gulp.watch('src/*.html', gulp.parallel('html'));
+    gulp.watch('src/js/*.js', gulp.parallel('script'))
+});
+
+gulp.task('build', gulp.series('clean', 'sass', 'export'));
+gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch'));
