e8c59fc46ba77f89c9428d161f6786e9c9bb4335
a.sorokin59@yandex.ru <Mesuti123>
2020-04-09 17:15:43 +0500

    add image, minify files, del pug

--- gulpfile.js
+++ gulpfile.js
@@ -7,21 +7,23 @@ var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     babel = require('gulp-babel'),
     uglify = require('gulp-uglify'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    htmlmin = require('gulp-htmlmin');
+    image = require('gulp-image');
 
 sass.compiler = require('node-sass');
 
-gulp.task('clean', async function () {
-    del.sync('dist')
-});
-
-gulp.task('php', function (done) {
-    return gulp.src('src/**/*.php')
-        .pipe(gulp.dest('dist'))
-        .pipe(browserSync.reload({stream: true}));
+gulp.task('clean', async function (done) {
+    del.sync('dist');
     done()
 });
 
+gulp.task('html', function (done) {
+    return gulp.src('./src/**/*.html')
+        .pipe(gulp.dest('./dist/'))
+        .pipe(browserSync.reload({stream: true}));
+    done()
+});
 gulp.task('lib-css', function (done) {
    return gulp.src([
             'node_modules/bootstrap/dist/css/bootstrap.css',
@@ -53,7 +55,6 @@ gulp.task('lib-js', function (done) {
 gulp.task('sass', function (done) {
     return gulp.src('src/scss/style.sass')
         .pipe(sass())
-        .pipe(cssnano())
         .pipe(autoprefixer({
             overrideBrowserslist: ['last 2 versions'],
             cascade: false
@@ -70,6 +71,52 @@ gulp.task('javascript', function (done) {
             presets: ['@babel/env']
         }))
         .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.reload({stream: true}));
+    done()
+});
+gulp.task('image', function (done) {
+    gulp.src('./src/img/**/*')
+        .pipe(image())
+        .pipe(gulp.dest('./dist/img/'));
+    done()
+});
+gulp.task('imagecopy', function (done) {
+    gulp.src('./src/img/**/*')
+        .pipe(gulp.dest('./dist/img/'));
+    done()
+});
+gulp.task('fonts', function (done) {
+    return gulp.src('src/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts/'))
+        .pipe(browserSync.reload({stream: true}));
+    done()
+});
+gulp.task('htmlminify', function (done) {
+    return gulp.src('./src/**/*.html', {dot: true, ignore: './src/func/**/*'})
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./dist/'))
+        .pipe(browserSync.reload({stream: true}));
+    done()
+});
+gulp.task('sassminify', function () {
+    return gulp.src('src/scss/**/*.sass')
+        .pipe(sass())
+        .pipe(cssnano())
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('javascriptminify', function (done) {
+    return gulp.src('src/js/**/*.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        //.pipe(rename({suffix: '.min'}))
         .pipe(uglify())
         .pipe(gulp.dest('dist/js'))
         .pipe(browserSync.reload({stream: true}));
@@ -85,10 +132,10 @@ gulp.task('browser-sync', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch('src/**/*.php', gulp.parallel('php'));
+    gulp.watch('src/**/*.html', gulp.parallel('html'));
     gulp.watch('src/scss/style.sass', gulp.parallel('sass'));
     gulp.watch('src/js/*.js', gulp.parallel('javascript'))
 });
 
-gulp.task('build', gulp.series('clean', 'php', 'lib-css', 'lib-js', 'sass', 'javascript'));
-gulp.task('default', gulp.parallel('php', 'lib-css', 'lib-js', 'sass', 'javascript', 'browser-sync', 'watch'));
+gulp.task('build', gulp.series('clean', 'htmlminify', 'lib-css', 'lib-js', 'sassminify', 'javascriptminify', 'image', 'fonts'));
+gulp.task('default', gulp.parallel('html', 'lib-css', 'lib-js', 'sass', 'javascript', 'imagecopy', 'fonts', 'browser-sync', 'watch'));
--- src/js/script.js
+++
@@ -1,5 +0,0 @@
-[1,2,3].map(n => n**2);
-let a = 2;
-console.log('Hello');
-
-$('button').on('click', () => alert('++'));
